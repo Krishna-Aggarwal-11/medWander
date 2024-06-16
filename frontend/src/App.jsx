8d68a6a2e9e1e5ac4fe954b194bec9e8d6633d08
Krishna-Aggarwal-11 <krishnaaggarwal11may@gmail.com>
2024-06-16 14:13:48 +0530

    frontend basic setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'flowbite-react'
+
+const App = () => {
+  return (
+    <div className='text-red-500 text-3xl'>
+      <Button>Hello</Button>
+    </div>
+  )
+}
+
+export default App
